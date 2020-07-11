3533a954286e8174c2dbaad912c65c9167408acd
elenamlopez <elena.mateos1@gmail.com>
2020-07-11 18:12:32 +0200

    Add first test for authentication

--- src/middlewares/authentication.spec.js
+++ src/middlewares/authentication.spec.js
@@ -1,3 +1,21 @@
+const authentication = require('./authentication')
 describe('Middlewares', () => {
-  describe('Authentication middleware', () => {});
+  describe('Authentication middleware', () => {
+    it('The user recived must have Id "1"', async () => {
+      const req = {
+        header = jest.fn().mockReturnValue('1')
+      }
+      const res = {
+        sendStatus: jest.fn()
+      } // se usa sólo si hay error pero esto se hace en otro test. Ahora se conprueba sólo que no se llama.
+      const next = jest.fn()
+      
+      await authentication(req, res, next) 
+      expect(req.header.mock.calls).toEqual([
+        ['user_id'] // al no tener un array vacío se confirma que se llama una vez y tiene user_id en la cabecera.
+      ])
+      expect(res.sendStatus.mock.calls).toEqual([])
+      expect(next.mock.calls).toEqual([[]])
+    });
+  });
 });
