0e6995ec844ccd929f26a89ba18c9da0bd6ccc5f
elenamlopez <elena.mateos1@gmail.com>
2020-07-12 16:34:12 +0200

    Add trwo error test if posts user don't exist

--- src/endpoints/index.spec.js
+++ src/endpoints/index.spec.js
@@ -5,7 +5,7 @@ describe('Endpoints', () => {
     it('Make a post', async () => {
       const mockUsers = [{ id: '1' }, { id: '2' }];
       const mockPost = {
-        userId: 1,
+        userId: '1',
         title: 'Título',
         body: 'Cuerpo del post',
       };
@@ -30,6 +30,32 @@ describe('Endpoints', () => {
       expect(axios.post.mock.calls).toEqual([
         ['https://jsonplaceholder.typicode.com/posts', mockPost],
       ]);
+    });
+
+    it('Should throw an error if the user to whom you want to assign a post don´t exist', async () => {
+      const mockUsers = [{ id: '1' }, { id: '2' }];
+      const mockPost = {
+        userId: '3',
+        title: 'Título',
+        body: 'Cuerpo del post',
+      };
+      const req = {
+        body: mockPost,
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn(),
+        sendStatus: jest.fn(),
+      };
+      const axios = {
+        get: jest.fn().mockResolvedValue({ data: mockUsers }),
+        post: jest.fn().mockResolvedValue({ data: { id: 1000 } }),
+      };
+
+      await postHandlers({ axios }).post(req, res);
+
+      expect(axios.post.mock.calls).toEqual([]);
+      expect(res.sendStatus.mock.calls).toEqual([[404]]);
     });
   });
 });
